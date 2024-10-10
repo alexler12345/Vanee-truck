931422056ee8d2cc138813eeb2ee74dfa45dc4bc
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2024-10-10 17:20:02 +0000

    product

--- product/productScript.js
+++ product/productScript.js
@@ -10,17 +10,6 @@ if (x.className === "topnav") {
 }
 
 
- // Function to toggle the visibility of descriptions
- function toggleDescription(element) {
-  const description = element.nextElementSibling;
 
-  if (description.classList.contains("active-description")) {
-    description.classList.remove("active-description");
-  } else {
-    // Collapse all descriptions first
-    document.querySelectorAll(".description").forEach((desc) => {
-      desc.classList.remove("active-description");
-    });
-    // Then expand the clicked one
-    description.classList.add("active-description");
-  }}
+
+
