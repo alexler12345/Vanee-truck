13531317bd0e1ee9259d991f8e619c11c4c44ed0
alexler12345 <156706923+alexler12345@users.noreply.github.com>
2024-10-27 15:17:34 -0600

    f

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,14 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { firstBucket, secondBucket } from './storage/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
   data,
-  storage
+  storage,
+  firstBucket,
+  secondBucket
 });
--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,29 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'amplifyTeamDrive'
-});
+    name: 'amplifyTeamDrive',
+    access: (allow) => ({
+      'profile-pictures/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'picture-submissions/*': [
+        allow.authenticated.to(['read','write']),
+        allow.guest.to(['read', 'write'])
+      ],
+    })
+  });
+
+  export const firstBucket = defineStorage({
+    name: 'firstBucket',
+    isDefault: true, // identify your default storage bucket (required)
+  });
+  
+  export const secondBucket = defineStorage({
+    name: 'secondBucket',
+    access: (allow) => ({
+      'private/{entity_id}/*': [
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ]
+    })
+  })
